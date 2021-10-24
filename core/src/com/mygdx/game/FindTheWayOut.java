273de3c01cc8f803c43a6cc854b58c9538ba50ce
mengyuqiao <1156253960@qq.com>
2021-10-23 21:14:37 -0400

    Second Commit: change gyroscope to accelerator to meet the demand

--- core/src/com/mygdx/game/FindTheWayOut.java
+++ core/src/com/mygdx/game/FindTheWayOut.java
@@ -5,13 +5,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.awt.Font;
 
 public class FindTheWayOut extends ApplicationAdapter {
 	SpriteBatch batch;
-	boolean gyroscopeAvail;
+	boolean accelerometerAvail;
 	Texture img;
 	int x, y;
 	int dx, dy;
@@ -20,7 +22,7 @@ public class FindTheWayOut extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		gyroscopeAvail = Gdx.input.isPeripheralAvailable(Input.Peripheral.Gyroscope);
+		accelerometerAvail = Gdx.input.isPeripheralAvailable(Input.Peripheral.Accelerometer);
 		x = Gdx.graphics.getWidth()/2;
 		y = Gdx.graphics.getHeight()/2;
 		dx = 0;
@@ -30,21 +32,48 @@ public class FindTheWayOut extends ApplicationAdapter {
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+
+		changeGravity();
+
 		batch.begin();
-		if (gyroscopeAvail){
-			float gyroX = Gdx.input.getGyroscopeX();
-			System.out.println(gyroX);
-			float gyroY = Gdx.input.getGyroscopeY();
-			System.out.println(gyroY);
-			dx += (int)(gyroX) % 10;
-			dy += (int)(gyroY) % 10;
-			x = (x + dx) % Gdx.graphics.getWidth();
-			y = (y + dy) % Gdx.graphics.getHeight();
-			batch.draw(img, x, y);
-		}
+		batch.draw(img, x, y);
 		batch.end();
 	}
-	
+
+	private void changeGravity() {
+		if (accelerometerAvail){
+			float[] mat = new float[4 * 4];
+			Gdx.input.getRotationMatrix(mat);
+
+			Matrix4 m = new Matrix4(mat);
+
+			Quaternion q = m.getRotation(new Quaternion());
+			dy += ((int)((-1)*(q.x*10)))%10;
+			dx += ((int)(q.y*10))%10;
+			if (( x > Gdx.graphics.getWidth() - img.getWidth() && dx > 0) || ( x < 0 && dx < 0) ){
+				dx = 0;
+				if (x > Gdx.graphics.getWidth() - img.getWidth()){
+					x =  Gdx.graphics.getWidth() - img.getWidth();
+				}else {
+					x = 0;
+				}
+			}
+			if (( y > Gdx.graphics.getHeight() - img.getHeight() && dy > 0) || ( y < 0 && dy < 0) ){
+				dy = 0;
+				if (y > Gdx.graphics.getHeight() - img.getHeight()){
+					y =  Gdx.graphics.getHeight() - img.getHeight();
+				}else {
+					y = 0;
+				}
+			}
+			x = (x + dx) % Gdx.graphics.getWidth();
+			y = (y + dy) % Gdx.graphics.getHeight();
+
+
+			System.out.println(q);
+		}
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
